1fc3a9a2ccb70f2864de1f64cd24f03be22b8b19
VISHWA <vishwavardhan33@gmail.com>
2025-12-26 15:44:01 +0530

    Implement top K frequent elements algorithm

---
+++ Heaps/top k frequent elements.py
@@ -0,0 +1,11 @@
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        cnt = Counter(nums)
+        min_heap = []
+        
+        for num,freq in cnt.items():
+            heapq.heappush(min_heap,(freq,num))
+            if len(min_heap)>k:
+                heapq.heappop(min_heap)
+        return [num for freq,num in min_heap]
