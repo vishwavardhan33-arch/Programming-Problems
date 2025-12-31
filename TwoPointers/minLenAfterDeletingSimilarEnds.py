242c680136dcc89230462276e00059a51f68bd92
VISHWA <vishwavardhan33@gmail.com>
2025-12-31 15:10:40 +0530

    Create minLenAfterDeletingSimilarEnds.py

---
+++ TwoPointers/minLenAfterDeletingSimilarEnds.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        i, j = 0, len(s) - 1
+        while i < j and s[i] == s[j]:
+            c = s[i]  
+            while i <= j and s[i] == c:
+                i += 1
+            while i <= j and s[j] == c:
+                j -= 1
+
+        return max(0,j - i + 1)
